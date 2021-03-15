c3ab03e4c1b33fa766f1de9edfd89e6063756205
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-15 20:02:01 +0100

    calculator

--- 5-calculator/solution.js
+++ 5-calculator/solution.js
@@ -2,11 +2,21 @@ let myOperator = prompt(
   "Which operation do you want to do? Type one of these: +, -, *, / or type exit to leave. You can also leave by cancelling the prompt"
 );
 
-if (myOperator === "exit") {
-  alert("Goodbye!");
-} else if (myOperator === "+") {
+if (myOperator === "+") {
+  let operand1 = Number(prompt("Type a first number"));
+  let operand2 = Number(prompt("Type a second number"));
+  alert(`The total is ${operand1 + operand2}`);
 } else if (myOperator === "-") {
+  let operand1 = Number(prompt("Type a first number"));
+  let operand2 = Number(prompt("Type a second number"));
+  alert(`The total is ${operand1 - operand2}`);
 } else if (myOperator === "*") {
+  let operand1 = Number(prompt("Type a first number"));
+  let operand2 = Number(prompt("Type a second number"));
+  alert(`The total is ${operand1 * operand2}`);
 } else if (myOperator === "/") {
-} else if (myOperator === "%") {
-}
+  let operand1 = Number(prompt("Type a first number"));
+  let operand2 = Number(prompt("Type a second number"));
+  alert(`The total is ${operand1 / operand2}`);
+} else myOperator === "exit";
+alert("Goodbye!");
