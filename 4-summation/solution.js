3f13004a2bdaf073147a59c9b77bcb22fbd169f3
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-15 19:16:18 +0100

    if else statement in summation

--- 4-summation/solution.js
+++ 4-summation/solution.js
@@ -4,19 +4,19 @@ let myArr = [];
 
 if (userNumber < 0) {
   alert("Try again!");
+} else {
+  for (let i = 0; i <= userNumber; i++) {
+    myArr.push(i);
+    // let finalAnswer = (i += i);
+
+    // let finalNumber = prompt(`Your summated number is ${finalAnswer}`);
+  }
+
+  console.log(myArr);
+
+  for (let j = myArr.length - 1; j >= 0; j--) {
+    finalNumber += myArr[j];
+  }
+
+  console.log(alert(` Your summated number is ${finalNumber}`));
 }
-
-for (let i = 0; i <= userNumber; i++) {
-  myArr.push(i);
-  // let finalAnswer = (i += i);
-
-  // let finalNumber = prompt(`Your summated number is ${finalAnswer}`);
-}
-
-console.log(myArr);
-
-for (let j = myArr.length - 1; j >= 0; j--) {
-  finalNumber += myArr[j];
-}
-
-console.log(alert(` Your summated number is ${finalNumber}`));
